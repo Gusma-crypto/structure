a85c7dd29bd10893f405b1c2ce5e739bfdc947de
Gusma-crypto <agussulistionoa0@gmail.com>
2025-04-17 16:36:54 +0700

    readme addd

---
+++ bubleshort.js
@@ -0,0 +1,15 @@
+let arr = [7,3,9,1,5];
+
+function bubleshort(){
+    let n =arr.length;
+    for(let i =0; i<n; i++){
+        // console.log(arr[i]);
+        for(let j =0; j<n-i; j++){
+            console.log(arr[i]);
+        }
+    }
+    
+    
+}
+
+bubleshort();
